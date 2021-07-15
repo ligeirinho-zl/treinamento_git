5b7a6a53d82a2c8781eb315f99aa6234168b1a41
ligeirinho-zl <jefferson.dos_santos@hotmail.com>
2021-07-15 08:24:32 -0300

    Iniciando o Lab

---
+++ viceversion/servicos.php
@@ -0,0 +1,302 @@
+  
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Vice Version Traduções</title>
+	
+     <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/font-awesome.css" rel="stylesheet">
+	
+	<link href="_css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="_css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="_css/font-awesome.css" rel="stylesheet">
+	<link href='./images/logo_vice.png' rel='icon' type='image/x-icon'/> 
+	<!-- for-mobile-apps -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	 <meta name="description" content="A Vice Version preza pela excelência ao atender às exigências de seus parceiros.Para isso, contamos com uma equipe altamente experiente e capacitada que oferecerá as melhores soluções às suas necessidades."/>
+	<meta name="keywords" content="traduções, traduções simples, traduções técnica, interpretação, revisão de textos, legendagem , transcrição, localização, locução, translate, vice-version, vice, version " />
+	<meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="1 day">
+    <meta name="language" content="Portuguese">
+
+    <meta itemprop="name" content="Vice Version Traduções"/>
+            <meta itemprop="alternateName" content="Vice Version"/>
+            <meta itemprop="description" content="A Vice Version preza pela excelência ao atender às exigências de seus parceiros.Para isso, contamos com uma equipe altamente experiente e capacitada que oferecerá as melhores soluções às suas necessidades."/>
+	<meta name="keywords" content="traduções, traduções simples, traduções técnica, interpretação, revisão de textos, legendagem , transcrição, localização, locução, translate, vice-version, vice, versi"/>
+            <meta itemprop='keywords' content='traduções, traduções simples, traduções técnica, interpretação, revisão de textos, legendagem , transcrição, localização, locução, translate, vice-version, vice, version'/>
+            <link itemprop="url" rel="https://www.viceversion.com.br" href="https://www.viceversion.com.br/">
+            
+
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Vice Version - Traduções" />
+        <meta property="og:description" content="Escritório especializado em tradução juramentada para cidadania italiana e serviços consulares junto aos Consulados Italianos de Curitiba e São Paulo." />
+         
+        <meta property="og:url" content="https://www.viceversion.com.br/" />
+        <meta property="og:site_name" content="Vice Version" />
+        <meta property="og:locale" content="pt_BR" />
+                <meta property="fb:admins" content="Vice Version" />
+        <meta property="article:author" content="https://www.facebook.com/Viceversion" />
+        <meta property="article:publisher" content="https://www.facebook.com/ViceVersion" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //for-mobile-apps -->
+	
+	 <!--Bloquear Copias -->
+<SCRIPT LANGUAGE="JavaScript"> 
+<!-- Disable 
+function disableselect(e){ 
+return false 
+} 
+
+function reEnable(){ 
+return true 
+} 
+
+//if IE4+ 
+document.onselectstart=new Function ("return false") 
+document.oncontextmenu=new Function ("return false") 
+//if NS6 
+if (window.sidebar){ 
+document.onmousedown=disableselect 
+document.onclick=reEnable 
+} 
+//--> 
+</script> 
+</head>
+
+<body>
+	<!-- banner -->
+	<div class="banner-main">
+		<div class="banner-2">
+			
+			<!--header-->
+			<div class="header">
+				<div class="container">
+					<nav class="navbar navbar-default">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+							 
+						</div>
+						<!--navbar-header-->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="quem-somos.php">Empresa</a></li>
+								<li><a href="clientes.php">Clientes</a></li>
+								 <li><a href="servicos.php" class="active" >Serviços</a></li>
+
+								<li><a href="contato.php">Contato</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+				 
+			<!--//header-->
+	</div>
+	</div>
+	<!-- //banner --><!-- Codes -->
+	 <section class="what_you w3-about py-5">
+		<div class="container py-md-4 mt-md-3">
+			<h3 class="w3l-titles"><img src="images/logo_vice.png" alt="" /> Nossos Serviços </h3>
+			<span class="w3-line black"></span>
+			<div class="row about-info-grids mt-md-5 pt-5">
+				<div class="col-md-4 about-info about-info1">
+					 
+					<h4>TRADUÇÃO SIMPLES </h4>
+					<div class="h4-underline"></div>
+					<p>Os textos indicados à tradução simples possuem os mais variados conteúdos e propósitos. Porém, como regra geral, costumam ser marcados pela falta ou pequena quantidade de conteúdo técnico. Suas informações podem ser compreendidas por todos, independente da ausência de formação ou treinamento especializados do indivíduo.
+					 </p>
+				</div>
+				<div class="col-md-4 about-info about-info2">
+				 
+					<h4>TRADUÇÃO TÉCNICA</h4>
+					<div class="h4-underline"></div>
+					<p>Muitas vezes o conteúdo de certos textos é caracterizado por procedimentos técnicos os quais deverão ser entendidos e seguidos de forma cuidadosa.Nestes casos, recomenda-se a tradução técnica.Possuímos amplo conhecimento e experiência em traduções técnicas dos mais diversos setores da indústria, do comércio e em prestação de serviços.</p>
+				</div>
+				<div class="col-md-4 about-info about-info3">
+				 
+					<h4>INTERPRETAÇÃO</h4>
+					<div class="h4-underline"></div>
+					<p>A interpretação é caracterizada pela tradução oral entre dois idiomas. Visando a excelência na execução,a interpretação deve ser realizada de modo preciso e discreto,demonstrando fidelidade às informações apresentadas e contribuindo para uma comunicação eficiente.Nossa equipe de intérpretes é altamente treinada e se adapta com perfeição às exigências específicas de cada evento.</p>
+				</div>
+			</div>
+			<div class="bord"></div>
+			<div class="row about-info-grids">
+				<div class="col-md-4 about-info about-info1">
+			 
+					<h4>REVISÃO DE TEXTOS</h4>
+					<div class="h4-underline"></div>
+					<p> A escrita reflete de maneira direta sobre certas características de uma empresa, tais como seu perfil, qualificações e confiabilidade. Um texto bem revisado garantirá que a mensagem contenha, além do máximo nível de precisão, fluidez na escrita, facilitando a compreensão da ideia pelo receptor.Estamos à sua disposição para revisar seu texto no que diz respeito ao conteúdo e gramática.</p>
+				</div>
+				<div class="col-md-4 about-info about-info2">
+					 
+					<h4>LEGENDAGEM</h4>
+					<div class="h4-underline"></div>
+					<p>A reprodução textual do conteúdo oral em vídeos tem se tornado cada vez mais comum à medida que a necessidade de se obter informações rápidas e objetivas tem influenciado continuamente as tendências atuais na indústria de produção multimídia.Conte com nossa ampla experiência em legendar vídeos de diversos segmentos, sempre de acordo com as regras comuns de legendagem seguidas pelos mais renomados estúdios da atualidade. </p>
+				</div>
+				<div class="col-md-4 about-info about-info3">
+					 
+					<h4>TRANSCRIÇÃO</h4>
+					<div class="h4-underline"></div>
+					<p>A transcrição é uma técnica que consiste na reprodução textual de um conteúdo oral normalmente apresentado em arquivos de áudio ou vídeo. Este processo é importante para facilitar a consulta, análise, armazenamento e exibição destas informações ao serem registradas em texto.Colocamos à sua disposição profissionais altamente experientes na compreensão oral de conteúdos exibidos em palestras, entrevistas, apresentações e outras ocasiões.  </p>
+				</div>
+			</div>
+	</div>
+</section>
+
+<!-- //services -->
+<!-- news -->
+	<section class="wthree-row w3-about py-5">
+		<div class="container py-md-4 mt-md-3">
+			<h3 class="heading-agileinfo"> <span>Abaixo tem mais</span></h3>
+			<span class="w3-line black"></span>
+			<div class="card-deck mt-md-5 pt-5">
+				  <div class="card">
+					<img src="images/g7.jpg" class="img-fluid" alt="Card image cap">
+					<div class="card-body w3ls-card">
+					  <h5 class="card-title">LOCALIZAÇÃO</h5>
+					  <p class="card-text mb-3 ">Chamamos de localização o processo de adaptar um produto ou serviço a uma determinada cultura. Isto inclui modificar conforme necessário não apenas o próprio idioma, mas também outros elementos para que fiquem adequados às características locais.Normalmente empregada na tradução de sites, softwares, games, entre outros, a localização garante que seu produto ou serviço fique completamente ajustado à região para a qual ele está destinado. Com o objetivo de assegurar a adaptação ideal, empregamos tradutores nativos que harmonizarão com perfeição os recursos de seu produto ou serviço à cultura desejada.</p>
+					</div>
+					
+				  </div>
+				  <div class="card">
+					<img src="images/g6.jpg" class="img-fluid" alt="Card image cap">
+					<div class="card-body w3ls-card">
+					  <h5 class="card-title">LOCUÇÃO</h5>
+					   <p class="card-text mb-3 ">Com o avanço da tecnologia na produção de conteúdo multimídia, a locução e a narração tornaram-se indispensáveis em diversos segmentos audiovisuais.Seja para dublagem e voice-over, treinamentos corporativos, e-learning em geral, bem como locução nos mais diversos tipos de áudio e vídeo, a Vice Version possui um casting abrangente de profissionais da voz, incluindo atores e dubladores, para atender a sua demanda.Conte com nossas vozes nativas, masculinas ou femininas, narrando nos mais variados idiomas para enriquecer seu projeto e dar aquele toque profissional que só a Vice Version pode conferir.</p>
+					</div>
+					
+				  </div>
+				  <div class="card">
+					<img src="images/g8.jpg" class="img-fluid" alt="Card image cap">
+					<div class="card-body w3ls-card">
+					  <h5 class="card-title">Nossos Clientes:</h5>
+					   <p class="card-text mb-3">Ser um parceiro Vice Version é ter a garantia de um atendimento personalizado, a mais estrita confidencialidade e respeito à qualidade e à pontualidade. </p>
+					</div>
+					
+				  </div>
+				</div>
+            </div>
+        </section>
+
+	
+<!-- footer -->
+<div class="footer">
+		<div class="container">
+			<div class="col-md-5 footer-grids">
+				<h3>Receba as Novidades</h3>
+				<p>Preencha seu e-mail e receba as novidades</p>
+				<form action="receba-as-novidades.php" method="post">
+					<input type="email" name="teste" placeholder="E-mail" required="">
+					<input type="submit" value="">
+				</form> 
+			</div>
+			<div class="col-md-4 footer-grids">
+				<h3>Sobre Nós</h3>
+				<p>A Vice Version preza pela excelência ao atender às exigências de seus parceiros. Para isso, contamos com uma equipe altamente experiente e capacitada que oferecerá as melhores soluções às suas necessidades.</p>
+			</div> 
+			<div class="col-md-3 footer-grids"> 
+				<h3>Contatos</h3>
+				<p>Santo André - SP/Brasil<br>
+				<span>Phone:(011)98986-0449</span>
+				<span>Phone:(011)98987-3820</span>
+				
+				<span>E-mail: <a href="#">contato@viceversion.com.br</a></span>
+				</p>
+			</div>
+			<div class="clearfix"> </div> 
+		</div>
+	</div>
+	<!-- footer -->
+	<div class="copyright">
+		<div class="container">
+			<p>© 2019 | Desenvolvido pela <a href="#">Update Informática</a></p>
+			<ul class="social-icons3">
+				<li><a href="https://www.facebook.com/viceversion/" class="fa fa-facebook icon-border facebook" target="_blank" > </a></li>
+				<li><a href="https://www.instagram.com/viceversion/" class="fa fa-instagram icon-border twitter" target="_blank" > </a></li>
+			 
+			</ul>
+		</div>
+	</div>
+	<!-- //footer -->
+<!-- bootstrap-pop-up -->
+					<div class="modal video-modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModal">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+										Improve
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>	
+								</div>
+								<section>
+									<div class="modal-body">
+										<img src="images/g3.jpg" alt=" " class="img-responsive" />
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+									</div>
+								</section>
+							</div>
+						</div>
+					</div>
+					<!-- //bootstrap-pop-up -->
+	<!-- js-scripts -->
+	<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+	<!-- //js -->
+	<!-- smooth scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+	<!-- //here ends scrolling icon -->
+<!-- //smooth scrolling -->
+<!-- scrolling script -->
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script> 
+<!-- //scrolling script -->
+	<!--search jQuery-->
+	<script src="js/main.js"></script>
+	<!--//search jQuery-->
+	<!-- smooth scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- //smooth scrolling -->
+	<!-- js-scripts -->
+</body>
+</html>
